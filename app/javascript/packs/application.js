47ee2b54f220d04f306254d32905b238286de20c
keenchang <s1277865@gmail.com>
2022-11-30 17:24:19 +0800

    會員系統-第三方登入-FaceBook

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,7 +9,7 @@ import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 import 'bootstrap/dist/js/bootstrap'
 import 'bootstrap/dist/css/bootstrap'
-import 'stylesheets/application'
+// import 'stylesheets/application'
 
 Rails.start()
 Turbolinks.start()
