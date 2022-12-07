e655b5e88d93db6f8ff202ced9240b02fc38d30b
david <62385136+wang-626@users.noreply.github.com>
2022-12-07 10:39:33 +0800

    Add path to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,19 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
-  devise_for :users, :controllers => { omniauth_callbacks: "users/omniauth_callbacks" }, controllers: { sessions: "users/sessions" }
-  
+  devise_for :users, controllers: { omniauth_callbacks: "users/omniauth_callbacks", sessions: "users/sessions" }
 
   root "movies#index"
-  resources :movies, only: [:index, :show]
-  resources :news, only: [:index, :show]
-  resources :theaters, only: [:index, :show]
+  resources :movies, only: %i[index show]
+  resources :news, only: %i[index show]
+  resources :theaters, only: %i[index show]
   namespace :admin do
-    resources :users ,only: [:index , :edit , :update ,:delete ]
+    resources :users, only: %i[index edit update delete]
     resources :movies
     resources :showtimes
     resources :news
     resources :theaters
   end
   namespace :"api/v1" do
-  end  
+  end
 end
