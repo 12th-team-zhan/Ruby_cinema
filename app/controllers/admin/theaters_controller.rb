c58bbb7c96396a7c912bdf48e899df8a2c077f10
david <62385136+wang-626@users.noreply.github.com>
2022-12-07 11:30:39 +0800

    Update theaters_controller.rb
    以修正跟組員確認風格

--- app/controllers/admin/theaters_controller.rb
+++ app/controllers/admin/theaters_controller.rb
@@ -1,48 +1,48 @@
-class Admin::TheatersController < ApplicationController
-  before_action :find_theater, only: [:show, :edit, :update, :destroy]
+module Admin
+  TheatersController < ApplicationController
+    before_action :find_theater, only: [:show, :edit, :update, :destroy]
 
-  def index
-    @theaters = Theater.all
-  end
-  
-  def show
-  end
-  
-  def new
-    @theater = Theater.new
-  end
-  
-  def create
-    @theater = Theater.new(params_theater)
-    if @theater.save
-      redirect_to admin_theaters_path, notice: "新增影城"
-    else
-      render :new
+    def index
+      @theaters = Theater.all
     end
-  end
   
-  def edit
-  end
+    def show; end
   
-  def update
-    if @theater.update(params_theater)
+    def new
+     @theater = Theater.new
+    end
+  
+    def create
+      @theater = Theater.new(params_theater)
+      if @theater.save
+       redirect_to admin_theaters_path, notice: "新增影城"
+      else
+        render :new
+      end
+    end
+  
+   def edit; end
+  
+    def update
+      if @theater.update(params_theater)
+        redirect_to admin_theaters_path
+      else
+        render :edit
+      end
+    end
+  
+    def destroy
+      @theater.destroy
       redirect_to admin_theaters_path
-    else
-      render :edit
     end
-  end
   
-  def destroy
-    @theater.destroy
-    redirect_to admin_theaters_path
-  end
+    private  
+    def find_theater
+      @theater = Theater.find(params[:id])
+    end
   
-  private  
-  def find_theater
-    @theater = Theater.find(params[:id])
-  end
-  
-  def params_theater
-    params.require(:theater).permit(:name, :address, :phone, :deleted_at)
+    def params_theater
+      params.require(:theater).permit(:name, :address, :phone, :deleted_at)
+    end
   end
 end
