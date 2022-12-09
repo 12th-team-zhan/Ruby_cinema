950e5c865aca2e50ae5ac26a7577cde333933b71
keenchang <s1277865@gmail.com>
2022-12-09 12:30:07 +0800

    修改影廳列表頁面，修改影廳新增頁面

--- app/controllers/admin/cinemas_controller.rb
+++ app/controllers/admin/cinemas_controller.rb
@@ -1,24 +1,26 @@
-class Admin::CinemasController < ApplicationController
+module Admin
+  class Admin::CinemasController < ApplicationController
     def index
-        @cinemas = Cinema.all
+      @cinemas = Cinema.all
     end
 
     def new
-        @cinema = Cinema.new
+      @cinema = Cinema.new
     end
 
     def create
-        @cinema = Cinema.new(cinema_params)
+      @cinema = Cinema.new(cinema_params)
 
-        if @cinema.save
-            redirect_to new_admin_cinema_seat_path(@cinema), notice: '成功新增影廳'
-        else
-            render(:new)
-        end
+      if @cinema.save
+        redirect_to new_admin_cinema_seat_path(@cinema), notice: '成功新增影廳'
+      else
+        render(:new)
+      end
     end
 
     private
     def cinema_params
-        params.require(:cinema).permit(:name, :max_row, :max_column)
+      params.require(:cinema).permit(:name, :max_row, :max_column)
     end
+  end
 end
