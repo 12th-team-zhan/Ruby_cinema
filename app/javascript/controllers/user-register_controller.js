1b81c79d88b26c49cd2ed9c201155b6cdb4d7e8d
Charsin Chan <116936412+charsin1olly@users.noreply.github.com>
2022-12-09 10:38:50 +0800

    PR#17修正

--- app/javascript/controllers/user-register_controller.js
+++ app/javascript/controllers/user-register_controller.js
@@ -1,51 +1,40 @@
-
-import { Controller } from "stimulus"
+import { Controller } from "stimulus";
 
 export default class extends Controller {
-  static targets = [ "benefits", "terms", "register","termsConfirmed"]
+  static targets = ["benefits", "terms", "register", "termsConfirmed"];
 
   connect() {
-    this.termsConfirmedTarget.classList.toggle("disabled")
-    
-    if (document.querySelector(".field_with_errors")){
-      this.benefitsTarget.classList.toggle("d-none")
-      this.termsTarget.classList.toggle("d-none")
-      this.registerTarget.classList.toggle("d-block")
-    }else{
-      this.benefitsTarget.classList.toggle("d-block")
-      this.termsTarget.classList.toggle("d-none")
-      this.registerTarget.classList.toggle("d-none")  
-    }
-      
-  }
-
-  confirm(){
-    if(document.getElementById("TermsCheckbox").checked){
-      this.termsConfirmedTarget.classList.add("disabled")
-      this.termsConfirmedTarget.classList.replace("disabled","action")
-    }else{
-      this.termsConfirmedTarget.classList.add("action")
-      this.termsConfirmedTarget.classList.replace("action","disabled")
+    if (document.querySelector(".field_with_errors")) {
+      this.benefitsTarget.classList.replace("d-block", "d-none");
+      this.registerTarget.classList.replace("d-none", "d-block");
     }
   }
 
-  benefitsNext(){
-    this.benefitsTarget.classList.replace("d-block","d-none")
-    this.termsTarget.classList.replace("d-none","d-block")
+  confirm() {
+    if (document.getElementById("TermsCheckbox").checked) {
+      this.termsConfirmedTarget.classList.replace("disabled", "action");
+    } else {
+      this.termsConfirmedTarget.classList.replace("action", "disabled");
+    }
   }
 
-  termsBack(){
-    this.termsTarget.classList.replace("d-block","d-none")
-    this.benefitsTarget.classList.replace("d-none","d-block")
+  benefitsNext() {
+    this.benefitsTarget.classList.replace("d-block", "d-none");
+    this.termsTarget.classList.replace("d-none", "d-block");
   }
 
-  termsNext(){
-    this.termsTarget.classList.replace("d-block","d-none")
-    this.registerTarget.classList.replace("d-none","d-block")
+  termsBack() {
+    this.termsTarget.classList.replace("d-block", "d-none");
+    this.benefitsTarget.classList.replace("d-none", "d-block");
   }
 
-  registerBack(){
-    this.registerTarget.classList.replace("d-block","d-none")
-    this.termsTarget.classList.replace("d-none","d-block")
+  termsNext() {
+    this.termsTarget.classList.replace("d-block", "d-none");
+    this.registerTarget.classList.replace("d-none", "d-block");
+  }
+
+  registerBack() {
+    this.registerTarget.classList.replace("d-block", "d-none");
+    this.termsTarget.classList.replace("d-none", "d-block");
   }
 }
