b5e8ab56e7415555a8c3b6b7de10a0d3a6cf9f6a
aegon <z851039@gmail.com>
2022-12-13 17:09:47 +0800

    add showtime crud & fix conflict

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks', sessions: 'users/sessions' },
-                     skip: :sessions
-  as :user do
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks', sessions: 'users/sessions' }, skip: :sessions as :user do
     post '/users/sign_in', to: 'devise/sessions#create', as: :user_session
     delete '/users/sign_out', to: 'devise/sessions#destroy', as: :destroy_user_session
   end
@@ -17,8 +15,10 @@ Rails.application.routes.draw do
       patch :cancel
     end
   end
+
   namespace :admin do
     resources :users, only: %i[index edit update delete]
+    
     resources :movies do
       member do
         delete :delete_images
@@ -28,11 +28,14 @@ Rails.application.routes.draw do
     end
     resources :news
     resources :theaters
+    
     resources :cinemas do
       resources :seats, only: %i[index new create]
     end
+    
     resources :orders
   end
+
   namespace :api do
     namespace :v1 do
       get 'getMovieList', to: 'getdata#movie_list'
