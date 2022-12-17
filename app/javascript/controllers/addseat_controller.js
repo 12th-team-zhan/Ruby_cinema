5657ac6bbd6c43406ce34926e1cebed189a050d3
david <62385136+wang-626@users.noreply.github.com>
2022-12-17 15:06:16 +0800

    Fix conflict

--- app/javascript/controllers/addseat_controller.js
+++ app/javascript/controllers/addseat_controller.js
@@ -30,55 +30,58 @@ export default class extends Controller {
                     }
                 }
             }
-
-            changeSeatStatus(el) {
-                const seatId = +el.target.dataset.seatId;
-                let seatStatus = el.target.dataset.status;
-
-                switch (seatStatus) {
-                    case "not added":
-                        el.target.classList.remove("bg-transparent")
-
-                        el.target.dataset.status = "added";
-
-                        const index = this.seatsNotAdded.indexOf(seatId);
-                        this.seatsNotAdded.splice(index, 1);
-                        break;
-                    case "added":
-                        el.target.classList.add("bg-transparent");
-
-                        el.target.dataset.status = "not added";
-
-                        this.seatsNotAdded.push(seatId);
-                        break;
-                    default:
-                        console.log("We don't have the seat status");
-                }
-            }
-
-            addToTable() {
-                const token = document.querySelector("meta[name='csrf-token']").content;
-                const cinemaId = this.element.dataset.id;
-
-                this.seatsAdded = this.seatsAll.filter(s => !this.seatsNotAdded.includes(s));
-                const seats = { "added": this.seatsAdded, "notAdded": this.seatsNotAdded }
-
-                fetch(`/admin/cinemas/${cinemaId}/seats`, {
-                    method: "POST",
-                    headers: {
-                        "X-csrf-Token": token,
-                        "Content-Type": "application/json"
-                    },
-                    redirect: 'follow',
-                    body: JSON.stringify(seats)
-                })
-                    .then((resp) => {
-                        if (resp.redirected) {
-                            window.location.href = resp.url;
-                        }
-                    })
-                    .catch((err) => {
-                        console.log(err);
-                    });
-            }
         }
+    }
+
+    changeSeatStatus(el) {
+        const seatId = +el.target.dataset.seatId;
+        let seatStatus = el.target.dataset.status;
+
+        switch (seatStatus) {
+            case "not added":
+                el.target.classList.remove("bg-transparent")
+
+                el.target.dataset.status = "added";
+
+                const index = this.seatsNotAdded.indexOf(seatId);
+                this.seatsNotAdded.splice(index, 1);
+                break;
+            case "added":
+                el.target.classList.add("bg-transparent");
+
+                el.target.dataset.status = "not added";
+
+                this.seatsNotAdded.push(seatId);
+                break;
+            default:
+                console.log("We don't have the seat status");
+        }
+    }
+
+
+    addToTable() {
+        const token = document.querySelector("meta[name='csrf-token']").content;
+        const cinemaId = this.element.dataset.id;
+
+        this.seatsAdded = this.seatsAll.filter(s => !this.seatsNotAdded.includes(s));
+        const seats = { "added": this.seatsAdded, "notAdded": this.seatsNotAdded }
+
+        fetch(`/admin/cinemas/${cinemaId}/seats`, {
+            method: "POST",
+            headers: {
+                "X-csrf-Token": token,
+                "Content-Type": "application/json"
+            },
+            redirect: 'follow',
+            body: JSON.stringify(seats)
+        })
+            .then((resp) => {
+                if (resp.redirected) {
+                    window.location.href = resp.url;
+                }
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+    }
+}
