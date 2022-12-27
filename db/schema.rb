6ead8986ef1d559263cfebbb0889c8eb704d2d30
Charsin Chan <116936412+charsin1olly@users.noreply.github.com>
2022-12-27 20:28:47 +0800

    調整後台切版

--- db/schema.rb
+++ db/schema.rb
@@ -90,6 +90,7 @@ ActiveRecord::Schema.define(version: 2022_12_27_065056) do
     t.bigint "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.text "youtube_iframe"
     t.index ["user_id"], name: "index_movies_on_user_id"
   end
 
@@ -132,6 +133,15 @@ ActiveRecord::Schema.define(version: 2022_12_27_065056) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["deleted_at"], name: "index_showtimes_on_deleted_at"
+  end
+
+  create_table "theater_showtimes", force: :cascade do |t|
+    t.bigint "showtime_id", null: false
+    t.bigint "theater_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["showtime_id"], name: "index_theater_showtimes_on_showtime_id"
+    t.index ["theater_id"], name: "index_theater_showtimes_on_theater_id"
   end
 
   create_table "theaters", force: :cascade do |t|
@@ -183,11 +193,13 @@ ActiveRecord::Schema.define(version: 2022_12_27_065056) do
     t.datetime "locked_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "fb_uid"
-    t.string "fb_token"
     t.string "name"
     t.datetime "deleted_at"
     t.integer "role", default: 0
+    t.string "fb_uid"
+    t.string "fb_token"
+    t.string "google_uid"
+    t.string "google_token"
     t.string "provider"
     t.string "uid"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
@@ -205,4 +217,6 @@ ActiveRecord::Schema.define(version: 2022_12_27_065056) do
   add_foreign_key "movies", "users"
   add_foreign_key "news", "users"
   add_foreign_key "orders", "users"
+  add_foreign_key "theater_showtimes", "showtimes"
+  add_foreign_key "theater_showtimes", "theaters"
 end
