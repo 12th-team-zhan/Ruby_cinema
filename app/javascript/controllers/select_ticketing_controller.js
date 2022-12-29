bc9139610f90431695c0c523a6cc18aec93c3360
Charsin Chan <116936412+charsin1olly@users.noreply.github.com>
2022-12-29 16:04:12 +0800

    fix select_tickets not work

--- app/javascript/controllers/select_ticketing_controller.js
+++ app/javascript/controllers/select_ticketing_controller.js
@@ -19,42 +19,40 @@ export default class extends Controller {
     let params = new URLSearchParams(location.search);
     this.showtimeId = params.get("showtimeid");
   }
+
   select(e) {
+    console.log(e.path[2].children[0].textContent);
     e.path[2].children[2].textContent =
       "$" +
       Number(e.srcElement.value) *
-      Number(e.path[2].children[0].textContent.substring(1));
+        Number(e.path[2].children[0].textContent.substring(1));
     this.calcAllTotal();
     this.calcAllAmount();
   }
 
   calcAllTotal() {
-    let allPrice = 0
-    this.allTotalTarget.textContent = ""
+    let allPrice = 0;
+    this.allTotalTarget.textContent = "";
     this.totalTargets.forEach((e) => {
-      allPrice = allPrice + Number(e.textContent.substring(1))
-    })
-    this.allTotalTarget.textContent = "$" + allPrice
+      allPrice = allPrice + Number(e.textContent.substring(1));
+    });
+    this.allTotalTarget.textContent = "$" + allPrice;
     if (allPrice > 0) {
-      this.nextTarget.classList.remove("d-none")
-    }
-    else {
-      this.nextTarget.classList.add("d-none")
-
+      this.nextTarget.classList.remove("d-none");
+    } else {
+      this.nextTarget.classList.add("d-none");
     }
   }
   calcAllAmount() {
-    let allAmount = 0
-    this.allAmountTarget.textContent = ""
+    let allAmount = 0;
+    this.allAmountTarget.textContent = "";
     this.amountTargets.forEach((e) => {
-      allAmount = allAmount + Number(e.value)
-    })
-    this.allAmountTarget.textContent = allAmount
-    this.changeLink(allAmount)
-
+      allAmount = allAmount + Number(e.value);
+    });
+    this.allAmountTarget.textContent = allAmount;
+    this.changeLink(allAmount);
   }
   changeLink(amount) {
-    console.log(this.regularAmountTarget.value);
     this.nextTarget.href = `/ticketing/select_seats?showtimeid=${this.showtimeId}&amount=${amount}&regularAmount=${this.regularAmountTarget.value}&concessionAmount=${this.concessionAmountTarget.value}&elderlyAmount=${this.elderlyAmountTarget.value}&disabilityAmount=${this.disabilityAmountTarget.value}`;
   }
 }
