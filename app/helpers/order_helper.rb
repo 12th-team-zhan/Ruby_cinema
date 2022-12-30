43aa8eeb5e5cd2d39fd9f31245bc1385a2021d22
david <62385136+wang-626@users.noreply.github.com>
2022-12-30 14:50:14 +0800

    Mosidy Order view

--- app/helpers/order_helper.rb
+++ app/helpers/order_helper.rb
@@ -2,10 +2,10 @@
 
 module OrderHelper
   def order_list(order)
-    html = "<p>訂單序號: #{order.serial} </p>
-            <p>電影名稱: #{order.tickets.first.showtime.movie.name}</p>
-            <p>影城: #{order.tickets.first.showtime.cinema.theater.name}</p>
-            <p>影廳: #{order.tickets.first.showtime.cinema.name}</p>"
+    html = "<p'>訂單序號: #{order.serial} </p>
+            <p'>電影名稱: #{order.tickets.first.showtime.movie.name}</p>
+            <p'>影城: #{order.tickets.first.showtime.cinema.theater.name}</p>
+            <p'>影廳: #{order.tickets.first.showtime.cinema.name}</p>"
     seat_list = ''
 
     order.tickets.each do |ticket|
@@ -16,8 +16,9 @@ module OrderHelper
       seat_list += "#{(col + 64).chr}排#{seat_id}號"
     end
 
-    html += "<p>座位: #{seat_list} </p>
-                   <p>開演時間: #{order.tickets.first.showtime.started_at.strftime('%Y/%m/%d %H:%M')}</p>"
+    html += "<p'>座位: #{seat_list} </p>
+             <p'>開演時間: #{order.tickets.first.showtime.started_at.strftime('%Y/%m/%d %H:%M')}</p>
+             <p'>總計金額: #{order.amount}</p>"
     html.html_safe
   end
 end
