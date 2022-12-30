ebf87419001a07abffeae9e7d6652e34065b4242
david <62385136+wang-626@users.noreply.github.com>
2022-12-30 14:49:01 +0800

    Modiify admin controller verify

--- app/controllers/admin/admin_controller.rb
+++ app/controllers/admin/admin_controller.rb
@@ -3,6 +3,20 @@
 module Admin
   class AdminController < ApplicationController
     layout 'admin_application'
+    before_action :user_signed_in
     before_action :current_user_is_staff
+
+    def user_signed_in
+      return if current_user.present?
+
+      redirect_to users_sign_in_path
+    end
+
+    def current_user_is_staff
+      return if current_user.admin? || current_user.staff?
+
+      render file: "#{Rails.root}/public/404.html", layout: false,
+             status: :bad_request
+    end
   end
 end
