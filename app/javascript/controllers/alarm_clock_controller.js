37ef81b3c066c414af752fb54cd1c2b990df5c66
david <62385136+wang-626@users.noreply.github.com>
2023-01-04 10:36:44 +0800

    Fix alert js

--- app/javascript/controllers/alarm_clock_controller.js
+++ app/javascript/controllers/alarm_clock_controller.js
@@ -15,7 +15,7 @@ export default class extends Controller {
 
             if (time === 0) {
                 clearInterval(countDown)
-                window.location.href = "http://127.0.0.1:3000/";
+                window.location.href = window.location.origin;
             }
         }, 1000);
     }
