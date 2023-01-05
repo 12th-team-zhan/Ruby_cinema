af1c457a56aa77914f9ee5d7f21cc5f8718e2d24
david <62385136+wang-626@users.noreply.github.com>
2023-01-05 10:46:21 +0800

    Change devise after_sign_in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,27 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  after_action :store_action
+
   rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
 
   private
 
+  def store_action
+    return unless request.get?
+    if (request.path != '/users/sign_in' &&
+        request.path != '/users/sign_up' &&
+        request.path != '/users/password/new' &&
+        request.path != '/users/password/edit' &&
+        request.path != '/users/confirmation' &&
+        request.path != '/users/sign_out' &&
+        !request.xhr?) # don't store ajax calls
+      store_location_for(:user, request.fullpath)
+    end
+  end
+
   def record_not_found
     render file: "#{Rails.root}/public/404.html", status: :bad_request, layout: false
   end
+
 end
